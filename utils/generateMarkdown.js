573992ca619ead9e2cbe83a78a8fa3710554e513
Bozhou Gong <robingbz@gmail.com>
2020-11-14 18:32:12 -0700

    generatemarkdown finished

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.Name}
+  
+  #Description
+  ${data.Description}
+
+  #Table of Contents
+  * [Installation](#Install)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributing](#Contributing)
+  * [Test](#Run)
+  * [Questions](#Questions)
+  
+  #Installation
+  ${data.Install}
+
+  #Usage
+  ${data.Usage}
+
+  #License
+  ${data.License}
+
+  #Cuntributing
+  ${data.Contributing}
+
+  #Tests
+  ${data.Run}
+
+  #Question
+  Contact me:
+  GitHub:${data.Username}
+  Email:${data.Email}
 
 `;
 }
