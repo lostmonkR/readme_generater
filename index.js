4f7e3d8d61c4d744001c9307c482d8c4a57a6eac
Bozhou Gong <robingbz@gmail.com>
2020-11-14 17:25:41 -0700

    const question

---
+++ index.js
@@ -0,0 +1,60 @@
+// array of questions for user
+const questions = [
+    {
+        type:"input",
+        message:"What is your GitHub username?",
+        name:"UserName",
+    },
+    {
+        type:"input",
+        message:"What is your email address?",
+        name:"Email",
+    },
+    {
+        type:"input",
+        message:"What is your project's name?",
+        name:"Title",
+    },
+    {
+        type:"input",
+        message:"Please write a short description of your project",
+        name:"Descroption",
+    },
+    {
+        type:"input",
+        message:"What kind of license should your project have?",
+        name:"License",
+    },
+    {
+        type:"input",
+        message:"What command should be run to install dependencies?",
+        name:"Install",
+    },
+    {
+        type:"input",
+        message:"What cpmman should be run to run tests?",
+        name:"Run",
+    },
+    {
+        type:"input",
+        message:"What does the user need to know about using the repo?",
+        name:"Using",
+    },
+    {
+        type:"input",
+        message:"What does the user need to know about contributing to the repo?",
+        name:"Contributing",
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
